6ac7fe2fd62a0ee78020a523e4b0100f4dc9ad24
Md. Fahim Bhuiyan <67233830+md-fahim-bhuiyan@users.noreply.github.com>
2023-08-25 20:30:45 +0600

    update

--- attendence_sys/admin.py
+++ attendence_sys/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 
 from .models import *
 
-# Register your models here.
-
 admin.site.register(Faculty)
 admin.site.register(Student)
 admin.site.register(Attendence)
