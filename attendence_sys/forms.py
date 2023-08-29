0338e2b4dc75cf98c0ac7fb8dfcbdb26b3b0dc3e
Md. Fahim Bhuiyan <67233830+md-fahim-bhuiyan@users.noreply.github.com>
2023-08-29 23:17:41 +0600

    update

--- attendence_sys/forms.py
+++ attendence_sys/forms.py
@@ -20,3 +20,4 @@ class FacultyForm(ModelForm):
         super(FacultyForm, self).__init__(*args, **kwargs)
         for visible in self.visible_fields():
             visible.field.widget.attrs['class'] = 'form-control'    
+            
